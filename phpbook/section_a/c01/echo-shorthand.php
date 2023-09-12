df716c34cdac54f1b9ba0ceb027c52894a1a0bbd
michiwo <mk1104kado@gmail.com>
2023-09-11 20:21:23 -0700

    Add page47 TRY

--- phpbook/section_a/c01/echo-shorthand.php
+++ phpbook/section_a/c01/echo-shorthand.php
@@ -1,6 +1,6 @@
 <?php 
-$name      = 'Ivy';
-$favorites = ['Chocolate', 'Toffee', 'Fudge',];
+$name      = 'Mitsuhiro Kadota';
+$favorites = ['milk', 'Chocolate', 'Toffee', 'Fudge'];
 ?>
 <!DOCTYPE html>
 <html>
